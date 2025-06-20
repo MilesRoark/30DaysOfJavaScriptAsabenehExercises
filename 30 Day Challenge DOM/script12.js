3bdb8653fb2ec819927553d16fa0f9c0ff396d18
milesroark <babatunjia@gmail.com>
2025-06-20 13:32:04 -0400

    finished bio section

---
+++ 30 Day Challenge DOM/script12.js
@@ -0,0 +1,10 @@
+const htext = document.getElementById("htext");
+
+const languages = ["HTML", "C.S.S.", "JAVASCRIPT", "REACT"];
+let languagesIndex = 0;
+function cycleLanguages() {
+  languagesIndex = (languagesIndex + 1) % languages.length;
+  htext.innerText = languages[languagesIndex];
+}
+
+setInterval(cycleLanguages, 5000);
